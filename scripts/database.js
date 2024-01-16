bb2c04f2838f67c9724b7fdde590e99c1a91fe73
Lincoln Keesecker <lekeesecker@gmail.com>
2024-01-16 13:57:17 -0600

    added scripts

---
+++ scripts/database.js
@@ -0,0 +1,36 @@
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villains: [
+        {
+            id: 1,
+            name: "Bad Boy"
+        },
+        {
+            id: 2,
+            name: "Badder Boy"
+        },
+        {
+            id: 3,
+            name: "Baddest Boy"
+        }
+    ]
+}
+
+export const getHeroes = () => {
+    return database.heroes.map(hero => ({...hero}))
+}
+
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
+}
